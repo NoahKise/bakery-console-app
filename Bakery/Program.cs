7cdfbfc6e014d3464c1f434a6cfd5aaafb8d7364
noahkise <noah@kisefamily.com>
2024-01-19 10:35:19 -0800

    program.cs initial inputs and error handling set up

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,7 +1,44 @@
+using System;
+
 class Program
 {
   static void Main()
   {
-    // We'll add user interface code here.
+    Console.WriteLine("Welcome to the bakery");
+    GetBreadInput();
+
+    static void GetBreadInput()
+    {
+      Console.WriteLine("Enter bread amount");
+      string breadInputString = Console.ReadLine();
+      if (!int.TryParse(breadInputString, out int breadInput) || breadInput < 0)
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Please enter a valid positive intiger");
+        Console.ResetColor();
+        GetBreadInput();
+      }
+      else
+      {
+        int breadQuantity = int.Parse(breadInputString);
+        GetPastryInput();
+      }
+    }
+    static void GetPastryInput()
+    {
+      Console.WriteLine("Enter pastry amount");
+      string pastryInputString = Console.ReadLine();
+      if (!int.TryParse(pastryInputString, out int pastryInput) || pastryInput < 0)
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Please enter a valid positive intiger");
+        Console.ResetColor();
+        GetPastryInput();
+      }
+      else
+      {
+        int pastryQuantity = int.Parse(pastryInputString);
+      }
+    }
   }
 }
