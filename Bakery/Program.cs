25277a1f2ac1fc15a16d4bfc69e27ecb47e2a94b
noahkise <noah@kisefamily.com>
2024-01-19 10:53:08 -0800

    MVP reached

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,43 +1,64 @@
 using System;
+using Bakery.Models;
 
-class Program
+namespace Bakery
 {
-  static void Main()
-  {
-    Console.WriteLine("Welcome to the bakery");
-    GetBreadInput();
 
-    static void GetBreadInput()
+  class Program
+  {
+    private static int breadQuantity;
+    private static int breadPrice;
+    private static int pastryQuantity;
+
+    private static int pastryPrice;
+    static void Main()
     {
-      Console.WriteLine("Enter bread amount");
-      string breadInputString = Console.ReadLine();
-      if (!int.TryParse(breadInputString, out int breadInput) || breadInput < 0)
+      Console.WriteLine("Welcome to the bakery");
+      GetBreadInput();
+
+      static void GetBreadInput()
       {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Please enter a valid positive intiger");
-        Console.ResetColor();
-        GetBreadInput();
+        Console.WriteLine("Enter bread amount");
+        string breadInputString = Console.ReadLine();
+        if (!int.TryParse(breadInputString, out int breadInput) || breadInput < 0)
+        {
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.WriteLine("Please enter a valid positive intiger");
+          Console.ResetColor();
+          GetBreadInput();
+        }
+        else
+        {
+          breadQuantity = int.Parse(breadInputString);
+          breadPrice = Bread.PriceBread(breadQuantity);
+          GetPastryInput();
+        }
       }
-      else
+      static void GetPastryInput()
       {
-        int breadQuantity = int.Parse(breadInputString);
-        GetPastryInput();
+        Console.WriteLine("Enter pastry amount");
+        string pastryInputString = Console.ReadLine();
+        if (!int.TryParse(pastryInputString, out int pastryInput) || pastryInput < 0)
+        {
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.WriteLine("Please enter a valid positive intiger");
+          Console.ResetColor();
+          GetPastryInput();
+        }
+        else
+        {
+          pastryQuantity = int.Parse(pastryInputString);
+          pastryPrice = Pastry.PricePastry(pastryQuantity);
+          CheckOut();
+        }
       }
-    }
-    static void GetPastryInput()
-    {
-      Console.WriteLine("Enter pastry amount");
-      string pastryInputString = Console.ReadLine();
-      if (!int.TryParse(pastryInputString, out int pastryInput) || pastryInput < 0)
+
+      static void CheckOut()
       {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Please enter a valid positive intiger");
-        Console.ResetColor();
-        GetPastryInput();
-      }
-      else
-      {
-        int pastryQuantity = int.Parse(pastryInputString);
+        Order userOrder = new Order(breadPrice, pastryPrice);
+        Console.WriteLine($"You have ordered {breadQuantity} loaves of bread and {pastryQuantity} pastries.");
+        int orderTotal = userOrder.CalculateTotal();
+        Console.WriteLine($"Your total is ${orderTotal}");
       }
     }
   }
