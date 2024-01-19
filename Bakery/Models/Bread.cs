02cbffbabf916cd2f88ac9a35474cf52a5d61c1e
noahkise <noah@kisefamily.com>
2024-01-19 09:03:23 -0800

    pass bread test 3 getOneFree

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,10 +1,14 @@
+using System;
+
 namespace Bakery.Models
 {
   public class Bread
   {
     public static int PriceBread(int inputNumber)
     {
-      int breadPrice = inputNumber * 5;
+      int freeLoaves = (int)Math.Floor((double)inputNumber / 3);
+      int chargedLoaves = inputNumber - freeLoaves;
+      int breadPrice = chargedLoaves * 5;
       return breadPrice;
     }
   }
