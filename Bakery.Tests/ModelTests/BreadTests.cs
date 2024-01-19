a8d2c6bb9fc0752d163edb2d5939e090c6dcc910
noahkise <noah@kisefamily.com>
2024-01-19 08:34:07 -0800

    add boilerplate code, complete template

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System;
+
+// Setup for tests that need code to run beteween each test
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests : IDisposable
+  {
+    public void Dispose()
+    {
+      // Code inside this method is run between each test.
+    }
+    // Test methods go here
+  }
+}
---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+//Setup for standard tests
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    // Test methods go here
+
+    // Example Test
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      // we can also use the arrange, act, assert organization in any test. 
+      Assert.AreEqual(ExpectedResult, CodeToTest);
+    }
+
+    // Example Collection Test
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      // we can also use the arrange, act, assert organization in any test. 
+      CollectionAssert.AreEqual(ExpectedResult, CodeToTest);
+    }
+  }
+}
